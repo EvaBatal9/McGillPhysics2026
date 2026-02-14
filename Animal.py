291bdfa6698204016548fb0282c0eee02ece541d
maddysiwick <maddy.siwick@icloud.com>
2026-02-14 11:50:57 -0500

    added an animal class and support for multiple animals

---
+++ Animal.py
@@ -0,0 +1,19 @@
+class Animal:
+    name=None
+    sound=None
+    frequency=None
+    image=None
+    soundStrength=None
+    location=None
+    maze=None
+    orientation=None
+
+    def __init__(self,name,sound,frequency,image,soundStrength,location,maze,orientation):
+        self.name=name
+        self.sound=sound
+        self.frequency=frequency
+        self.image=image
+        self.soundStrength=soundStrength
+        self.location=location
+        self.maze=maze
+        self.orientation=orientation

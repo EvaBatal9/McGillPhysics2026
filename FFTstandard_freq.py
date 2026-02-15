965b59f31e5e309595839be3ed9dec32a2dc375a
maddysiwick <maddy.siwick@icloud.com>
2026-02-15 00:33:43 -0500

    im so tired

--- FFTstandard_freq.py
+++ FFTstandard_freq.py
@@ -2,6 +2,10 @@ import numpy as np
 import scipy.fft as sp
 import matplotlib.pyplot as plt
 from audiopressure import Duck_pressure, Cat_pressure, Cow_pressure, Dog_pressure, Donkey_pressure, Kathy_pressure, Lion_pressure, Monkey_pressure, Pig_pressure
+from mazeTest import animals
+
+for animal in animals:
+    print(animal.maze)
 
 #unpacking pressure tuples into arrays
 time, Duck_pressure = Duck_pressure
